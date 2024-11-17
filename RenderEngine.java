5784397d6f2e914aeb4aef970b7c3ba47f9c36ab
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2024-11-16 22:39:21 -0500

    sequential rendering with resolution scaling

--- RenderEngine.java
+++ RenderEngine.java
@@ -3,13 +3,27 @@ public abstract class RenderEngine {
     protected int width;
     protected int height;
     protected int maxIterations;
-    protected int boundary;
+    protected float boundary;
+    protected float resolutionScale = 1.0f; // 1 = full resolution, < 1 = lower resolution
 
-    public RenderEngine(int width, int height, int maxIterations, int boundary) {
+    public RenderEngine(int width, int height, int maxIterations, float boundary, float resolutionScale) {
         this.width = width;
         this.height = height;
         this.maxIterations = maxIterations;
         this.boundary = boundary;
+        this.resolutionScale = resolutionScale;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setResolutionScale(float scale) {
+        this.resolutionScale = scale;
     }
 
     public abstract int[][] computeMandelbrot(double xMin, double xMax, double yMin, double yMax);
